738fcf596d5fc474409eba892e7541f57fd8da65
loot <49004208+zvzi@users.noreply.github.com>
2022-11-09 08:42:17 -0500

    Update Spectatee.lua

--- src/Spectatee.lua
+++ src/Spectatee.lua
@@ -1,4 +1,4 @@
-` spectate `
+-- // Spectate
 -- to spectate someone, change the "t" in the 3rd line next to "Players" and replace the letter with your target
 getgenv().Target = game.Workspace.Players[t]
 getgenv().Targeting = true
@@ -6,6 +6,6 @@ game.Workspace.Camera.CameraSubject = Target.Character.HumanoidRootPart
 else
     Targeting = false
 end
-` stop spectating `
+-- // Stop Spectating
   local user = game.Players.LocalPlayer.Name
   game.Workspace.Camera.CameraSubject = game.Workspace.Players[user].Humanoid
