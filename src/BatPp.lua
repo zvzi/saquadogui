a263a3784efe7643885313437192e1b696163e5e
loot <49004208+zvzi@users.noreply.github.com>
2022-11-09 14:02:19 -0500

    Create BatPp.lua

---
+++ src/BatPp.lua
@@ -0,0 +1,7 @@
+	Plr.Character.Humanoid:UnequipTools()
+	wait(0.1)
+	Plr.Character.Humanoid:EquipTool(Plr.Backpack["[Bat]"])
+	Plr.Character.HumanoidRootPart.CFrame = CFrame.new(803.5, 48, -547, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+	Plr.Character["[Bat]"].Handle.Orientation = Vector3.new(0, 0, 90)
+	Plr.Character["[Bat]"].Handle.Position = Vector3.new(806, 47, -547)
+	Plr.Character.HumanoidRootPart.CFrame = LastPos
